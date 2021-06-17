ed4a925f50e53985c820584bfcaa593aadb3f0bd
David Mohr <david.mohr@ardexa.com>
2021-06-17 14:08:06 +1000

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+    <q-card class="q-pa-lg" v-for="user in users" v-bind:key="user">
+      {{ user }}
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      users: ['1', '2', '3']
+    }
+  },
+  async created () {
+    const res = await this.$api.get('/api/members')
+    console.log(res.data)
+    this.users = res.data
+  }
+}
+</script>
