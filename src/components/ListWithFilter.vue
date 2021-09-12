b5d45d339bd0512451b0fbdc0c3853910d88b6ad
David Mohr <david.mohr@ardexa.com>
2021-09-12 13:48:45 +1000

    refactor: abstract component from members page (for use in users page)

---
+++ src/components/ListWithFilter.vue
@@ -0,0 +1,77 @@
+<template>
+  <q-page class="flex justify-center" style="margin-top: 30px">
+    <div
+      v-if="!!items"
+      class="row"
+    >
+      <div class="col">
+        <q-input
+          v-model="search"
+          filled
+          type="search"
+          hint="Search"
+          @update:modelValue="page = 1"
+        >
+          <template #append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+        <q-list
+          bordered
+          separator
+          style="min-width: 300px"
+        >
+          <slot :items="matchingItems" />
+        </q-list>
+        <div
+          v-if="totalPages > 1"
+          class="q-pa-lg flex flex-center"
+        >
+          <q-pagination
+            v-model="page"
+            :max="totalPages"
+            :max-pages="5"
+            :direction-links="true"
+          />
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import fuzzysearch from 'fuzzysearch'
+
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true
+    },
+    key: {
+      type: String,
+      default: 'NAME'
+    }
+  },
+  data () {
+    return {
+      search: '',
+      page: 1,
+      pageSize: 15
+    }
+  },
+  computed: {
+    totalPages () {
+      return Math.ceil(this.filteredItems.length / this.pageSize)
+    },
+    matchingItems () {
+      return this.filteredItems
+        .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize)
+    },
+    filteredItems () {
+      if (!this.items) return []
+      return this.items.filter(items => fuzzysearch(this.search.toLowerCase(), items[this.key].toLowerCase()))
+    }
+  }
+}
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,83 +1,30 @@
 <template>
-  <q-page class="flex justify-center" style="margin-top: 30px">
-    <div
-      v-if="!!members"
-      class="row"
+  <list-with-filter v-slot="props" :items="members">
+    <q-item
+      v-for="member in props.items"
+      :key="member.ID"
+      v-ripple
+      clickable
+      :to="{ name: 'Member', params: { memberId: member.ID }}"
     >
-      <div class="col">
-        <q-input
-          v-model="search"
-          filled
-          type="search"
-          hint="Search"
-          @update:modelValue="page = 1"
-        >
-          <template #append>
-            <q-icon name="search" />
-          </template>
-        </q-input>
-        <q-list
-          bordered
-          separator
-          style="min-width: 300px"
-        >
-          <q-item
-            v-for="member in matchingMembers"
-            :key="member.ID"
-            v-ripple
-            clickable
-            :to="{ name: 'Member', params: { memberId: member.ID }}"
-          >
-            <q-item-section>{{ member.NAME }}</q-item-section>
-          </q-item>
-        </q-list>
-        <div
-          v-if="totalPages > 1"
-          class="q-pa-lg flex flex-center"
-        >
-          <q-pagination
-            v-model="page"
-            :max="totalPages"
-            :max-pages="5"
-            :direction-links="true"
-          />
-        </div>
-      </div>
-    </div>
-  </q-page>
+      <q-item-section>{{ member.NAME }}</q-item-section>
+    </q-item>
+  </list-with-filter>
 </template>
 
 <script>
-import fuzzysearch from 'fuzzysearch'
+import ListWithFilter from '../components/ListWithFilter.vue'
+
 export default {
-  data () {
-    return {
-      search: '',
-      page: 1,
-      pageSize: 15
-    }
-  },
+  components: { ListWithFilter },
   computed: {
     members () {
       return this.$store.state.members.members
-    },
-    totalPages () {
-      return Math.ceil(this.filteredMembers().length / this.pageSize)
-    },
-    matchingMembers () {
-      return this.filteredMembers()
-        .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize)
     }
   },
   async created () {
     if (!this.members.length) {
       await this.$store.dispatch('members/getMembers')
-    }
-  },
-  methods: {
-    filteredMembers () {
-      if (!this.members) return []
-      return this.members.filter(member => fuzzysearch(this.search.toLowerCase(), member.NAME.toLowerCase()))
     }
   }
 }
