ad6a0f0dc29e0204e44c1026f6fdb1f79526d304
David Mohr <david.mohr@ardexa.com>
2021-09-12 15:01:57 +1000

    feat(api): add user endpoint

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { query } = require('./database');
+const { encryptPassword, query } = require('./database');
 
 const auth = require('./auth');
 const signup = require('./signup');
 
 const router = express.Router();
+
+const roles = ['user', 'coordinator', 'admin'];
 
 function hasRole (role) {
   return (req, res, next) => {
@@ -61,6 +63,27 @@ router.get('/users', hasRole('admin'), async (req, res) => {
   try {
     const { results } = await query('SELECT id, username, role from auth');
     res.send(results);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(500);
+  }
+});
+
+router.post('/users', hasRole('admin'), async (req, res) => {
+  try {
+    if (!roles.includes(req.body.role)) {
+      return res.status(400).send({ error: 'Invalid role' });
+    }
+    if (!/^[a-z0-9]+$/i.test(req.body.username)) {
+      return res.status(400).send({ error: 'Invalid username' });
+    }
+    if (typeof req.body.password === 'string' && req.body.password.length < 8) {
+      return res.status(400).send({ error: 'Invalid password' });
+    }
+    const { key, salt } = await encryptPassword(req.body.password);
+    const { results } = await query('INSERT INTO auth (username, password, salt, role) VALUES(?, ?, ?, ?)', [req.body.username, key, salt, req.body.role]);
+    console.log('RESULTS', results);
+    res.sendStatus(200);
   } catch (err) {
     console.log(err);
     return res.sendStatus(500);
