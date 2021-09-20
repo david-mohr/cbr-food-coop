ba6b66fbf623cd8150e14f66ced5612da2053427
David Mohr <david.mohr@ardexa.com>
2021-09-20 16:09:31 +1000

    feat: add postgres definitions of tables

---
+++ mock/tables.sql
@@ -0,0 +1,67 @@
+CREATE TABLE IF NOT EXISTS signup (
+  id SERIAL PRIMARY KEY  NOT NULL,
+  name           TEXT    NOT NULL,
+  email          TEXT    NOT NULL,
+  phone          TEXT    NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS customers (
+    id varchar(255) PRIMARY KEY NOT NULL,
+    searchkey varchar(255) UNIQUE NOT NULL,
+    taxid varchar(255),
+    name varchar(255) NOT NULL,
+    card varchar(255),
+    maxdebt decimal NOT NULL DEFAULT '0',
+    address varchar(255),
+    address2 varchar(255),
+    postal varchar(255),
+    city varchar(255),
+    region varchar(255),
+    country varchar(255),
+    firstname varchar(255),
+    lastname varchar(255),
+    email varchar(255),
+    phone varchar(255),
+    phone2 varchar(255),
+    fax varchar(255),
+    notes varchar(255),
+    curdate timestamp with time zone,
+    curdebt decimal,
+    visible boolean NOT NULL DEFAULT false
+);
+
+CREATE TABLE IF NOT EXISTS members_extra (
+    id varchar(255) NOT NULL,
+    discvaliduntil timestamp with time zone,
+    anuaffiliation varchar(255),
+    incomelevel varchar(255),
+    sendemails boolean NOT NULL DEFAULT true,
+    membershipexpires timestamp with time zone,
+    hadfirstshop boolean NOT NULL DEFAULT false,
+    hadfreelunch boolean NOT NULL DEFAULT false,
+    discountstatus varchar(255),
+    discountrate decimal NOT NULL DEFAULT 20,
+    isapproved boolean NOT NULL DEFAULT false,
+    PRIMARY KEY (id),
+    CONSTRAINT members_extras_id FOREIGN KEY (id) REFERENCES customers (id)
+);
+
+CREATE TABLE IF NOT EXISTS members_history (
+    id varchar(255) NOT NULL,
+    datenew timestamp with time zone NOT NULL,
+    member varchar(255) NOT NULL,
+    action varchar(255),
+    amountpaid decimal,
+    notes varchar(255),
+    PRIMARY KEY (id),
+    CONSTRAINT member_history_customer FOREIGN KEY (member) REFERENCES customers (id)
+);
+
+CREATE TABLE IF NOT EXISTS auth (
+    id SERIAL NOT NULL,
+    username varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    salt varchar(32) NOT NULL,
+    role varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
