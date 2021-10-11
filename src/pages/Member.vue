eb2735d9e788f239302fbc3c7fd3e811019b26fc
David Mohr <david.mohr@ardexa.com>
2021-10-11 20:06:15 +1100

    refactor: move member-status to a new component

--- src/pages/Member.vue
+++ src/pages/Member.vue
@@ -1,155 +1,68 @@
 <template>
-  <q-page padding v-if="!member">
-    <p>Member not found</p>
-  </q-page>
-  <q-page padding v-if="!!member">
-    <div class="q-py-md row">
-      <member-details
-        :member="member"
-        class="col"
-      />
-    </div>
-    <div class="q-py-md row">
-      <q-card class="col">
-        <q-tabs
-          v-model="tab"
-          dense
-          class="text-grey"
-          active-color="primary"
-          indicator-color="primary"
-          align="justify"
-          narrow-indicator
-        >
-          <q-tab name="status" label="Status" />
-          <q-tab name="history" label="History" />
-        </q-tabs>
+  <q-page padding>
+    <template v-if="!memberId">
+      <p>Member not found</p>
+    </template>
+    <template v-else-if="!ready">
+      <q-spinner size="2rem" />
+    </template>
+    <template v-else>
+      <div class="q-py-md row">
+        <member-details
+          :member-id="memberId"
+          class="col"
+        />
+      </div>
+      <div class="q-py-md row">
+        <q-card class="col">
+          <q-tabs
+            v-model="tab"
+            dense
+            class="text-grey"
+            active-color="primary"
+            indicator-color="primary"
+            align="justify"
+            narrow-indicator
+          >
+            <q-tab name="status" label="Status" />
+            <q-tab name="history" label="History" />
+          </q-tabs>
 
-        <q-separator />
+          <q-separator />
 
-        <q-tab-panels v-model="tab" animated>
-          <q-tab-panel name="status">
-            <div v-if="!status">
-              <q-spinner color="primary" size="3em" />
-            </div>
-            <div class="q-pa-md" v-if="!!status">
-              <div class="row q-col-gutter-md">
-                <div class="col">
-                  <div class="q-pa-md">
-                  <q-btn label="Renew Membership" color="primary" @click="renewMembership = true"></q-btn>
-                  </div>
-                  <q-card bordered :class="expStatus">
-                    <q-card-section>
-                      <div class="text-h6">Membership expires</div>
-                    </q-card-section>
-                    <q-separator inset />
-                    <q-card-section>
-                      <div class="text-h6">{{ expDate }}</div>
-                    </q-card-section>
-                  </q-card>
-                </div>
-                <div class="col">
-                  <div class="q-pa-md">
-                  <q-btn label="Add Volunteering Hours" color="primary" @click="addVolunteering = true"></q-btn>
-                  </div>
-                  <q-card bordered :class="discountStatus">
-                    <q-card-section>
-                      <div class="text-h6">Discount valid until</div>
-                    </q-card-section>
-                    <q-separator inset />
-                    <q-card-section>
-                      <div class="text-h6">{{ discountDate }}</div>
-                    </q-card-section>
-                  </q-card>
-                </div>
-              </div>
-            </div>
-          </q-tab-panel>
-
-          <q-tab-panel name="history">
-            <div class="q-pa-md">
-              <q-table
-                title="History"
-                :rows="history"
-                :columns="columns"
-                row-key="name"
+          <q-tab-panels v-model="tab" animated>
+            <q-tab-panel name="status">
+              <member-status
+                :member-id="memberId"
               />
-            </div>
-          </q-tab-panel>
-        </q-tab-panels>
-      </q-card>
-    </div>
-      <div class="q-pa-md q-gutter-sm">
-    <q-dialog v-model="addVolunteering" persistent>
-      <q-card style="min-width: 350px">
-        <q-form
-          no-error-focus
-          @submit="onSubmit"
-          @reset="reset"
-        >
-        <q-card-section>
-          <div class="text-h6">Add Volunteer Hours</div>
-        </q-card-section>
-        <q-card-section class="q-pt-none">
-          <div class="text">Date</div>
-          <q-input
-            v-model="date"
-            filled
-            type="date"
-          />
-          <div class="text">Hours Worked</div>
-          <q-input
-            v-model="hours"
-            filled
-            type="number"
-            lazy-rules
-            :rules="[
-              val => val != null && val != '' || 'Please include the number of hours',
-              val => val > 0 && val <= 16 || 'Please add a reasonable number of hours'
-            ]"
-          />
-          <div class="text">Volunteering Type</div>
-          <q-input
-            v-model="activity"
-            filled
-            type="string"
-          />
-        </q-card-section>
-
-        <q-card-actions align="left" class="text-primary">
-          <q-btn flat
-            label="Cancel"
-            type="reset"
-            v-close-popup
-          />
-          <q-btn
-            flat
-            type="submit"
-            label="Add Volunteer Hours"
-          />
-        </q-card-actions>
-        </q-form>
-      </q-card>
-    </q-dialog>
-  </div>
+            </q-tab-panel>
+            <q-tab-panel name="history">
+              <div class="q-pa-md">
+                <q-table
+                  title="History"
+                  :rows="history"
+                  :columns="columns"
+                  row-key="name"
+                />
+              </div>
+            </q-tab-panel>
+          </q-tab-panels>
+        </q-card>
+      </div>
+    </template>
   </q-page>
 </template>
 
 <script>
-import { date } from 'quasar'
-import { DateTime } from 'luxon'
 import MemberDetails from '../components/MemberDetails.vue'
+import MemberStatus from '../components/MemberStatus.vue'
 
 export default {
-  components: { MemberDetails },
+  components: { MemberDetails, MemberStatus },
   data () {
     return {
-      renewMembership: false,
-      addVolunteering: false,
-      date: DateTime.now().toISODate(),
-      hours: 1.0,
-      activity: null,
+      ready: false,
       tab: 'status',
-      memberId: this.$route.params.memberId,
       columns: [
         { name: 'Date', align: 'left', label: 'Date', field: 'datenew', sortable: true },
         { name: 'event', align: 'left', label: 'Event', field: 'action', sortable: true },
@@ -158,91 +71,27 @@ export default {
       ]
     }
   },
-  methods: {
-    reset () {
-      this.date = DateTime.now().toISODate()
-      this.hours = 1.0
-      this.activity = null
-    },
-    async onSubmit (evt) {
-      // TODO: actually add this to the database.
-      console.log(this.date, this.hours, this.activity)
-      try {
-        await this.$api.post(`/api/members/${this.memberId}/history`, {
-          date: this.date,
-          action: 'Volunteered',
-          paid: this.hours,
-          notes: this.activity
-        }, {
-          headers: {
-            authorization: 'Bearer ' + this.$store.state.members.token
-          }
-        })
-        await this.$store.dispatch('members/getHistory', this.memberId)
-        await this.$store.dispatch('members/getStatus', this.memberId)
-        this.$q.notify({
-          color: 'green-4',
-          textColor: 'white',
-          icon: 'cloud_done',
-          message: 'Maybe Added!'
-        })
-      } catch (err) {
-        this.$q.notify({
-          color: 'red-4',
-          textColor: 'white',
-          icon: 'error',
-          message: 'Not Added'
-        })
-      }
-      this.reset()
-      this.addVolunteering = false
-    }
-  },
   computed: {
-    member () {
-      return this.$store.state.members.members.find(member => member.id === this.memberId)
+    memberId () {
+      return this.$route.params.memberId
     },
     history () {
       return this.$store.state.members.memberHistory[this.memberId]
-    },
-    status () {
-      return this.$store.state.members.memberStatus[this.memberId]
-    },
-    expDate () {
-      return date.formatDate(this.status.membershipexpires, 'YYYY-MM-DD')
-    },
-    expStatus () {
-      const now = new Date()
-      const monthBeforeExp = date.subtractFromDate(this.status.membershipexpires, { days: 31 })
-      if (now < monthBeforeExp) {
-        return 'bg-positive'
-      }
-      if (date.isBetweenDates(now, monthBeforeExp, this.status.membershipexpires, { onlyDate: true })) {
-        return 'bg-warning'
-      }
-      return 'bg-negative'
-    },
-    discountDate () {
-      return date.formatDate(this.status.discvaliduntil, 'YYYY-MM-DD')
-    },
-    discountStatus () {
-      const now = new Date()
-      if (now < this.status.discvaliduntil) {
-        return 'bg-positive'
-      }
-      return 'bg-negative'
     }
   },
   async created () {
+    const promises = []
     if (!this.$store.state.members.members.length) {
-      await this.$store.dispatch('members/getMembers')
+      promises.push(this.$store.dispatch('members/getMembers'))
     }
     if (!this.$store.state.members.memberHistory[this.memberId]) {
-      await this.$store.dispatch('members/getHistory', this.memberId)
+      promises.push(this.$store.dispatch('members/getHistory', this.memberId))
     }
     if (!this.$store.state.members.memberStatus[this.memberId]) {
-      await this.$store.dispatch('members/getStatus', this.memberId)
+      promises.push(this.$store.dispatch('members/getStatus', this.memberId))
     }
+    await Promise.all(promises)
+    this.ready = true
   }
 }
 </script>
