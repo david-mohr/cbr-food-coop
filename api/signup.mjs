2f0e88a75277098da875fb73202b50468f37ac39
David Mohr <david.mohr@ardexa.com>
2021-10-11 22:01:40 +1100

    wip: draft a new signup endpoint with all the values to populate a hybrid customer/member table

--- api/signup.mjs
+++ api/signup.mjs
@@ -3,9 +3,11 @@ import { query } from './database.mjs'
 
 const router = express.Router()
 
+const signupProps = ['firstname', 'lastname', 'address', 'postal', 'city', 'email', 'phone', 'anuaffiliation', 'incomelevel', 'sendemails']
+
 router.post('/signup', async (req, res) => {
   try {
-    await query('INSERT into signup(name, phone, email) values($1, $2, $3) RETURNING *', [req.body.name, req.body.phone, req.body.email])
+    await query(`INSERT into signup (${signupProps.join(', ')}) values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *`, signupProps.map(prop => req.body[prop]))
     res.sendStatus(200)
   } catch (err) {
     console.error(err)
--- mock/tables.sql
+++ mock/tables.sql
@@ -1,8 +1,15 @@
 CREATE TABLE IF NOT EXISTS signup (
   id SERIAL PRIMARY KEY  NOT NULL,
-  name           TEXT    NOT NULL,
-  email          TEXT    NOT NULL,
-  phone          TEXT    NOT NULL
+  firstname varchar(255),
+  lastname varchar(255),
+  address varchar(255),
+  postal varchar(255),
+  city varchar(255),
+  email varchar(255),
+  phone varchar(255),
+  anuaffiliation varchar(255),
+  incomelevel varchar(255),
+  sendemails boolean NOT NULL DEFAULT true,
 );
 
 CREATE TABLE IF NOT EXISTS customers (
