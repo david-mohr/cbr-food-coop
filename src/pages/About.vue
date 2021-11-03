bff1ef3385f3b46e2430b22c76f572253955ce7d
jonny <jonny@jonny.org>
2021-11-03 17:40:59 +1100

    feat: add version number to about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,10 +1,31 @@
 <template>
   <q-page>
-    <div class="text-h2 text-center">
-      About this
-    </div>
     <div class="text-h4 text-center">
-      software at the food coop
+      About this software
+    </div>
+    <div class="q-pa-md">
+      Version {{ version }}
+    </div>
+    <div class="q-pa-md">
+      Find the source code here:
+      <a href="https://github.com/david-mohr/cbr-food-coop">https://github.com/david-mohr/cbr-food-coop</a>
+    </div>
+    <div class="q-pa-md">
+      The tech team meets every Wednesday at 7pm.
+    </div>
+    <div class="q-pa-md">
+      Get in touch with us if you want to be involved: info@cbrfoodcoop.org.au
     </div>
   </q-page>
 </template>
+
+<script>
+import { version } from '../../package.json'
+
+export default {
+  data () {
+    return { version: version }
+  }
+}
+
+</script>
