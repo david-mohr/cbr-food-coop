fd848b369e87e67448f1d7fa26f02250a407d72d
David Mohr <david.mohr@ardexa.com>
2021-12-01 18:18:32 +1100

    refactor: lift vend user creation into a separate function

--- api/signup.mjs
+++ api/signup.mjs
@@ -44,28 +44,31 @@ async function createMailchimp (email) {
   })
 }
 
+async function createVend (member) {
+  const json = {
+    first_name: member.firstname,
+    last_name: member.lastname,
+    email: member.email,
+    mobile: member.phone,
+    physical_suburb: member.suburb,
+    physical_postcode: member.postcode,
+    physical_country_id: 'AU'
+  }
+  return got.post(`${VEND_URL}/customers`, {
+    headers: {
+      authorization: `Bearer ${process.env.VEND_API_KEY}`
+    },
+    json
+  }).json()
+}
+
 const VEND_URL = 'https://thefoodcooperativeshop.vendhq.com/api/2.0'
 router.post('/signups/:id/vend', hasRole('coordinator'), async (req, res) => {
   try {
     const results = await query('SELECT * from signup WHERE id = $1', [req.params.id])
     if (!Array.isArray(results) || !results.length) return res.sendStatus(404)
     if (results[0].vendid) return res.sendStatus(409)
-    const json = {
-      first_name: results[0].firstname,
-      last_name: results[0].lastname,
-      email: results[0].email,
-      mobile: results[0].phone,
-      physical_suburb: results[0].suburb,
-      physical_postcode: results[0].postcode,
-      physical_country_id: 'AU'
-    }
-    const newVendUser = await got.post(`${VEND_URL}/customers`, {
-      headers: {
-        authorization: `Bearer ${process.env.VEND_API_KEY}`
-      },
-      json
-    }).json()
-    console.log(newVendUser)
+    const newVendUser = await createVend(results[0])
     await query('UPDATE signup SET vendid = $1 WHERE id = $2', [newVendUser.data.id, req.params.id])
     res.json({ vendid: newVendUser.data.id })
   } catch (err) {
