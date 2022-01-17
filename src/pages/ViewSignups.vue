d10865720213f0dd6c18b4c03c3be34ae9c3e419
David Mohr <david.mohr@ardexa.com>
2022-01-17 18:34:47 +1100

    feat: add refresh button to signups list

--- src/pages/ViewSignups.vue
+++ src/pages/ViewSignups.vue
@@ -1,22 +1,31 @@
 <template>
   <q-page
-    class="flex justify-center"
     style="margin-top: 30px"
   >
-    <list-with-filter
-      v-slot="props"
-      :items="signups"
-    >
-      <q-item
-        v-for="signup in props.items"
-        :key="signup.id"
-        v-ripple
-        clickable
-        :to="{ name: 'Process signup', params: { signupId: signup.id }}"
+    <div class="row justify-center">
+      <q-btn
+        icon="refresh"
+        label="Refresh"
+        color="accent"
+        @click="getSignups"
+      />
+    </div>
+    <div class="row justify-center q-mt-md">
+      <list-with-filter
+        v-slot="props"
+        :items="signups"
       >
-        <q-item-section>{{ signup.name }}</q-item-section>
-      </q-item>
-    </list-with-filter>
+        <q-item
+          v-for="signup in props.items"
+          :key="signup.id"
+          v-ripple
+          clickable
+          :to="{ name: 'Process signup', params: { signupId: signup.id }}"
+        >
+          <q-item-section>{{ signup.name }}</q-item-section>
+        </q-item>
+      </list-with-filter>
+    </div>
   </q-page>
 </template>
 
@@ -30,8 +39,13 @@ export default {
       return this.$store.state.members.signups
     }
   },
-  async created () {
+  created () {
     if (!this.signups.length) {
+      this.getSignups()
+    }
+  },
+  methods: {
+    async getSignups () {
       await this.$store.dispatch('members/getSignups')
     }
   }
