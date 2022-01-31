38d795fe9bc8f5da8ddad91eb14209416e2fdfe8
David Mohr <david.mohr@ardexa.com>
2022-01-31 19:02:49 +1100

    feat: add single membership endpoint

--- api/memberships.mjs
+++ api/memberships.mjs
@@ -14,4 +14,15 @@ router.get('/', hasRole('coordinator'), async (req, res) => {
   }
 })
 
+router.get('/:id', hasRole('coordinator'), async (req, res) => {
+  try {
+    const membership = await query('SELECT * from memberships WHERE membership_id = $1', [req.params.id])
+    if (!membership.length) return res.sendStatus(404)
+    res.send(membership[0])
+  } catch (err) {
+    console.log(err)
+    return res.sendStatus(500)
+  }
+})
+
 export default router
