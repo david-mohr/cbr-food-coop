fc4a2bc94757668902582637663e778ddf0664ea
jamgb <jamesgbacon@gmail.com>
2022-02-08 16:47:46 +1100

    feat: discount avatars for members

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -15,6 +15,16 @@
         :to="{ name: 'Member', params: { memberId: member.id }}"
       >
         <q-item-section>{{ member.name }}</q-item-section>
+        <q-item-section avatar>
+          <q-avatar
+            rounded
+            :color="discColour(member.membership_id)"
+            text-color="white"
+            font-size="small"
+          >
+            {{ discStatus(member.membership_id) }}%
+          </q-avatar>
+        </q-item-section>
       </q-item>
     </list-with-filter>
   </q-page>
@@ -28,6 +38,21 @@ export default {
   computed: {
     members () {
       return this.$store.state.members.members
+    }
+  },
+  methods: {
+    membership: function (id) {
+      return this.$store.getters['members/membershipLookup'][id]
+    },
+    discStatus: function (id) {
+      const now = new Date()
+      const membership = this.membership(id)
+      const membershipExp = membership?.expires
+      const volDiscExp = membership?.discvaliduntil
+      return ((now < membershipExp) ? ((now < volDiscExp) ? 20 : 5) : 0)
+    },
+    discColour: function (id) {
+      return this.discStatus(id) > 5 ? 'green' : this.discStatus(id) > 0 ? 'orange' : 'red'
     }
   },
   async created () {
