93c9ceb5f10a16feb9d2958eac2db412913fa04d
jamgb <jamesgbacon@gmail.com>
2022-04-27 12:13:17 +1000

    adding ids to members_approval_sheets_members

--- api/approvals.mjs
+++ api/approvals.mjs
@@ -12,7 +12,7 @@ router.post('/', hasRole('admin'), async (req, res) => {
   const sheet = await query('INSERT INTO members_approval_sheets (id, datecreated, nummembers, dateapproved, approvedby, datesigned, signedby) values($4, $1, $2, $1, $3, $1, $3) RETURNING id', [date, members.length, req.body.approvedby, uid()])
 
   for (const member of members) {
-    await query('INSERT INTO members_approval_sheets_members (member, approvalsheet) values($1, $2)', [member, sheet[0].id])
+    await query('INSERT INTO members_approval_sheets_members (id, member, approvalsheet) values($3, $1, $2)', [member, sheet[0].id, uid()])
     await query('UPDATE customers SET approved = true WHERE id = $1', [member])
     await query('INSERT into members_history (id, datenew, member, action, amountpaid, notes) values($1, $2, $3, $4, $5, $6)', [uid(), date, member, 'Approved', null, req.body.notes])
     // await sendConfirmed(member)
