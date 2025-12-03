548c9b00e38783db3bffbceff275f7a01b4d26a4
James Bacon <james.bacon4@det.nsw.edu.au>
2025-12-03 22:23:15 +1100

    Update mailchimp recent expiry query

--- sql/mailchimp-recentexpiry.sql
+++ sql/mailchimp-recentexpiry.sql
@@ -14,22 +14,7 @@ LEFT JOIN members_extra me ON c.id = me.id
 WHERE 
   c.email IS NOT NULL 
   AND c.email != '' and c.email LIKE '%@%'
-  AND (m.expires <= CURRENT_TIMESTAMP and m.expires > (CURRENT_TIMESTAMP + '3 months'::interval))
+  AND (m.expires <= CURRENT_TIMESTAMP and m.expires > (CURRENT_TIMESTAMP - '3 months'::interval))
   AND COALESCE(me.sendemails, true) = true
-
-UNION
-
--- Prospective members (signup_members)
-SELECT 
-  sm.email AS "Email Address",
-  sm.firstname AS "First Name",
-  sm.lastname AS "Last Name", 
-  null AS "Address",
-  sm.phone AS "Phone Number"
-FROM signup_members sm
-WHERE 
-  sm.email IS NOT NULL
-  AND sm.email != '' and sm.email LIKE '%@%'
-  AND COALESCE(sm.sendemails, true) = true
 
 ORDER BY "Email Address";
